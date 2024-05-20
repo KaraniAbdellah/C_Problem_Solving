10909749ae7b5595a54fe816816be5d6ccb613a5
Abdellah <AbdellahKarani>
2024-05-20 16:43:58 +0100

    .

--- beecrowd/1033.c
+++ beecrowd/1033.c
@@ -2,36 +2,47 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <inttypes.h>
+
+
 int64_t fib(int64_t n) {
 	if (n <= 1) return n;
-	else {
-		int64_t W = 1, U = 0, V = 0;
-		for (int i = 2; i < n + 1; i+= 2) {
-			U = W + V;
-			V = W;
-			W = U;
-			if (n != 2) {
-				U = W + V;
-				V = W;
-				W = U;				
-			}
+	int64_t U = 0, V = 1, W;
+	for (int i = 2; i < n + 1; i += 2) {
+		W = U + V;
+		V = U;
+		U = W;
+		if (i != 2) {
+			W = U + V;
+			V = U;
+			U = W;
 		}
-		return U;
-	} 
+	}
+	printf("\nW = %jd\n", W);
+	return W;
 }
+
+int64_t getLastDigit(int64_t base, int64_t number) {
+    while (number >= base) {
+        number = number % base;
+    }
+    return number;
+}
+
 int main() {
-    int64_t n, base, count = 1;
+    int64_t n, base, count = 1, rem;
     while (1) {
 		if ((scanf("%" PRId64 " %" PRId64, &n, &base)) == EOF || (n == 0 && base == 0)) break;
-        // Print The Result
         int64_t result = fib(n + 1);
-        int64_t last_digit = (2 * result) - 1;
-        last_digit = (last_digit) % base;
-        // Print result with modulo
+        int64_t calls_nbr = (2 * result) - 1;
+        int64_t last_digit = getLastDigit(base, calls_nbr);
         printf("Case %" PRId64 ": %" PRId64 " %" PRId64 " %" PRId64 "\n", count, n, base, last_digit);
-
         count++;
     }
     return 0;
 }
+
+
+
+
+
 
