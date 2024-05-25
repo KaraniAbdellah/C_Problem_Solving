e550952ce58d1f4b03589e899a5988fbad7828e7
Abdellah <AbdellahKarani>
2024-05-25 11:57:58 +0100

    .

--- beecrowd/1036.c
+++ beecrowd/1036.c
@@ -6,15 +6,12 @@ int main() {
 	scanf("%lf %lf %lf", &a, &b, &c);
 	delta = pow(b, 2) - (4 * a * c);
 	if (a == 0 || delta < 0) {
-		printf("Impossivel calcular");
+		printf("Impossivel calcular\n");
 		return 0;
 	}
 	r1 = (-b - sqrt(delta)) / (2 * a);
 	r2 = (-b + sqrt(delta)) / (2 * a);
-	printf("R2 = %.5lf\n", r2);
-	printf("R1 = %.5lf", r1);
+	printf("R1 = %.5lf\n", r2);
+	printf("R2 = %.5lf\n", r1);
 	return 0;
 }
-
-
-
