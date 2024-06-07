068d1b13a4b3046e02e6cb6d3d65659af9964c63
Abdellah <AbdellahKarani>
2024-06-07 22:10:58 +0100

    .

--- beecrowd/1044.c
+++ beecrowd/1044.c
@@ -8,7 +8,7 @@ int main() {
         else printf("Nao sao Multiplos\n"); 
     } else  {
         if (a % b == 0) printf("Sao Multiplos\n");
-        else printf("Nao sao Multiplos\n");         
+        else printf("Nao sao Multiplos\n");
     }
     return 0;
 }
--- beecrowd/1045.c
+++ beecrowd/1045.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
-#include <math.h>
+
+
+// [a, b, c]
 
 int main() {
-    float a, b, c;
-    scanf("%f %f %f", &a, &b, &c);
-    if (a * a == b * b + c * c) {
-        printf("Perimetro = %.1f", a + b + c);
-    } else {
-        printf("Area = %.1f", b * a * b);
+    double a, b, c;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    // Sort In Descreasing Order
+    double temp;
+    if (a < b) { temp = a; a = b; b = temp; };
+    if (a < c) { temp = a; a = c; c = temp; };
+    if (b < c) { temp = c; c = b; b = temp; };
+    // printf("a = %lf, b = %lf, c = %lf\n", a, b, c);
+    // Print The Type of The Traingle
+    if (a >= b + c)  {
+        printf("NAO FORMA TRIANGULO\n");
+        return 0;
     }
+    if (a * a == b * b + c * c) printf("TRIANGULO RETANGULO\n");
+    if (a * a > b * b + c * c) printf("TRIANGULO OBTUSANGULO\n");
+    if (a * a < b * b + c * c) printf("TRIANGULO ACUTANGULO\n");
+    if (a == b && b == c && c == a) printf("TRIANGULO EQUILATERO\n");
+    if ((a == b && c != b) || (b == c && b != a) || (a == c && c != b)) printf("TRIANGULO ISOSCELES\n");
     return 0;
-    
 }
+
+
+
 
