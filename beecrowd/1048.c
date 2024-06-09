a049085d34ae630c1c71ce7c44ca945b3d8313a1
Abdellah <AbdellahKarani>
2024-06-10 00:13:07 +0100

    .

--- beecrowd/1048.c
+++ beecrowd/1048.c
@@ -1,38 +1,32 @@
     #include <stdio.h>
- 
- 0 - 400.00
-400.01 - 800.00
-800.01 - 1200.00
-1200.01 - 2000.00
-Above 2000.00
 
-15%
-12%
-10%
-7%
-4%
  
 int main() {    
     float salary, temp;
     scanf("%f", &salary);
-    if (salary <= 0 && salary >= 400) {
-        temp = salary;
-        printf("Novo salario: %.2f \n Reajuste ganho: %.2f\nEm percentual: %d %", (salary * 15) / 100, salary - temp, 15);
+    if (salary >= 0 && salary <= 400) {
+        temp = ((salary * 15) / 100) + salary;
+        printf("Novo salario: %.2f\nReajuste ganho: %.2f\nEm percentual: %d %%\n", temp, (float) 
+        temp - salary, 15);
     }
     else if (salary > 400 && salary <= 800) {
-        temp = salary;
-        printf("Novo salario: %.2f \n Reajuste ganho: %.2f\nEm percentual: %d %", (salary * 12) / 100, salary - temp, 12);
+        temp = ((salary * 12) / 100) + salary;
+        printf("Novo salario: %.2f\nReajuste ganho: %.2f\nEm percentual: %d %%\n", temp, (float) 
+        temp - salary, 12);
     }
-    else if (salary > 800 && salary >= 1200) {
-        temp = salary;
-        printf("Novo salario: %.2f \n Reajuste ganho: %.2f\nEm percentual: %d %", (salary * 10) / 100, salary - temp, 10);
+    else if (salary > 800 && salary <= 1200) {
+        temp = ((salary * 10) / 100) + salary;
+        printf("Novo salario: %.2f\nReajuste ganho: %.2f\nEm percentual: %d %%\n", temp, (float) 
+        temp - salary, 10);
     }
-    else if (salary > 1200 && salary >= 2000) {
-        temp = salary;
-        printf("Novo salario: %.2f \n Reajuste ganho: %.2f\nEm percentual: %d %", (salary * 7) / 100, salary - temp, 7);
+    else if (salary > 1200 && salary <= 2000) {
+        temp = ((salary * 7) / 100) + salary;
+        printf("Novo salario: %.2f\nReajuste ganho: %.2f\nEm percentual: %d %%\n", temp, (float) 
+        temp - salary, 7);
     } else {
-        temp = salary;
-        printf("Novo salario: %.2f \n Reajuste ganho: %.2f\nEm percentual: %d %", (salary * 7) / 100, salary - temp, 7);
+        temp = ((salary * 4) / 100) + salary;
+        printf("Novo salario: %.2f\nReajuste ganho: %.2f\nEm percentual: %d %%\n", temp, (float) 
+        temp - salary, 4);
     }
     return 0;
 }
