9e2d355e35aedabec7f6e17116347c5ced75eb5d
Abdellah <AbdellahKarani>
2024-06-12 00:05:44 +0100

    .

--- beecrowd/1052.c
+++ beecrowd/1052.c
@@ -3,18 +3,18 @@
 int main() {
     int month;
     scanf("%d", &month);
-    if (month == 1) printf("January");
-    else if (month == 2) printf("February");
-    else if (month == 3) printf("March");
-    else if (month == 4) printf("April");
-    else if (month == 5) printf("May");
-    else if (month == 6) printf("June");
-    else if (month == 7) printf("July");
-    else if (month == 8) printf("August");
-    else if (month == 9) printf("September");
-    else if (month == 10) printf("October");
-    else if (month == 11) printf("November");
-    else printf("December");
+    if (month == 1) printf("January\n");
+    else if (month == 2) printf("February\n");
+    else if (month == 3) printf("March\n");
+    else if (month == 4) printf("April\n");
+    else if (month == 5) printf("May\n");
+    else if (month == 6) printf("June\n");
+    else if (month == 7) printf("July\n");
+    else if (month == 8) printf("August\n");
+    else if (month == 9) printf("September\n");
+    else if (month == 10) printf("October\n");
+    else if (month == 11) printf("November\n");
+    else printf("December\n");
     return 0;
 }
 
