85a8f2abd8d6ec3c9b8562d5d05d7e8413951ca7
Abdellah <AbdellahKarani>
2024-06-14 20:36:12 +0100

    .

--- beecrowd/1061.c
+++ beecrowd/1061.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_the_result(char *str1, char *str2, int start, int end) {
+
+// The Problem Is With If hours == 24 or minutes or seconds
+
+void print_the_result(char *str1, char *str2, char *str, char *str0) {
     int hours, minutes, seconds;
-    int days = end - start;
-    int *temp[3];
-    // detecte the hours and minutes and second for two strings --> With Manual Method
-    int index[6] = {0, 1, 5, 6, 10, 11};
-    int count = 0;
-    for (int i = 0; i < 3; i++) {
-        temp[i] = malloc(sizeof(int) * 2);
-        temp[i][0] = atoi(&str1[index[count]]);
-        temp[i][1] = atoi(&str1[index[count + 1]]);
-        count += 2;
-    }
-    for (int i = 0; i < 3; i++) printf("%d %d\n", temp[i][0], temp[i][1]);
+    int days = atoi(&str0[4]) - atoi(&str[4]);
+    // Get The Hours
+    hours = atoi(&str2[0]) - atoi(&str1[0]);
+    if (hours < 0) hours *= -1;
+    hours = 24 - hours; 
+    // Get The Minutes
+    minutes = atoi(&str2[5]) - atoi(&str1[5]);
+    if (minutes < 0) minutes *= -1;
+    // Get The Seconds
+    seconds = atoi(&str2[9]) - atoi(&str1[9]);
+    if (seconds < 0) seconds *= -1;
+    printf("%d dia(s)\n%d hora(s)\n%d minuto(s)\n%d segundo(s)\n", days, hours, minutes, seconds);
 }
+
 
 int main() {
     int begin_event, end_event;
-    char str1[20], str2[20];
-    printf("Dia "); scanf("%d", &begin_event); getchar();
-    scanf("%[^\n]", str1);
-    printf("Dia "); scanf("%d", &end_event); getchar();
-    scanf("%[^\n]", str2);
-    print_the_result(str1, str2, begin_event, end_event);
+    char str1[20], str2[20], str[20], str0[20];
+    scanf("%[^\n]", str); getchar();
+    scanf("%[^\n]", str1); getchar();
+    scanf("%[^\n]", str0); getchar();
+    scanf("%[^\n]", str2); getchar();
+    print_the_result(str1, str2, str, str0);
     return 0;
 }
 
