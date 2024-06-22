3c8dfa393c1a77a8dd59c0eb57e8e6f9e70dc7c4
Abdellah <AbdellahKarani>
2024-06-22 23:52:52 +0100

    .

---
+++ codeforces/Vanya_and_Fence.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+int main() {
+    int nbr_friends, height_fence;
+    scanf("%d %d", &nbr_friends, &height_fence);
+    int T[1000];
+    for (int i = 0; i < nbr_friends; i++) {
+        scanf("%d", &T[i]);
+    }
+    return 0;
+}
+
