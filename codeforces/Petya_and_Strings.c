a7a69d76aa8b9c8b6a63387fa919f6caf33382f4
Abdellah <AbdellahKarani>
2024-07-07 23:14:35 +0100

    .

--- codeforces/Petya_and_Strings.c
+++ codeforces/Petya_and_Strings.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-int comparaision(char str1[], char str2[]) {
+
+
+
+void comparison(char str1[], char str2[]) {
     char *p = str1, *q = str2;
     int re;
-    for (; *p != '\0' ||  *q != '\0'; p++, q++) {
-        if ((int) *p > 90) *p = (char) ((int) *p - 32);
-        if ((int) *q > 90) *q = (char) ((int) *q - 32);
+    for (; *p != '\0' &&  *q != '\0'; p++, q++) {
+        if ((int) *p >= 97 && (int) *p <= 122) *p = (char) ((int) *p - 32);
+        if (*q >= 97 && *q <= 122) *q = (char) ((int) *q - 32);
         re = (int) *p - (int) *q;
-        // printf("p = %d \\ q = %d\n", (int) *p, (int) *q);
-        if (re != 0) return re;
+        if (re > 0) { printf("1"); return; }
+        else if (re < 0) { printf("-1"); return; }
+        else continue;
     }
-    return 0;
+    printf("0");
 }
 
 int main() {
     char str1[101], str2[101];
-    fgets(str1, 100, stdin);
-    fgets(str2, 100, stdin);
-    str1[101] = '\0'; str2[101] = '\0';
-    int re = comparaision(str1, str2);
-    if (re > 0) printf("1");
-    else if (re < 0) printf("-1");
-    else printf("0");
+    scanf("%[^\n]", str1); getchar();
+    scanf("%[^\n]", str2);
+    str1[strlen(str1)] = '\0'; // Remove newline character from str1
+    str2[strlen(str2)] = '\0'; // Remove newline character from str2
+    comparison(str1, str2);
     return 0;
 }
 
