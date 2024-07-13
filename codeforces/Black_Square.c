4c1027c32530bbf5161b94c0d6af75e9847ac39c
Abdellah <AbdellahKarani>
2024-07-13 18:57:14 +0100

    .

---
+++ codeforces/Black_Square.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // understand and then wrtting code
+    int n;
+    
+    
+    
+    return 0;
+}
+
--- codeforces/Police_Recruits.c
+++ codeforces/Police_Recruits.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    
+    int events, event, p = 0, re = 0;
+    scanf("%d", &events);
+    for (int i = 0; i < events; i++) {
+        scanf("%d", &event);
+        if (event == -1) {
+            if (p + event < 0) re++;
+            else p -= 1;
+        } else p += event;
+    }
+    printf("%d", re);
     return 0;
 }
 
