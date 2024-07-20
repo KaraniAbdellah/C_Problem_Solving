8c84e2defc417072a105ca7b5eb3c0482e997936
Abdellah <AbdellahKarani>
2024-07-20 19:26:47 +0100

    .

--- codeforces/Night_at_the_Museum.c
+++ codeforces/Night_at_the_Museum.c
@@ -1,44 +1,25 @@
 #include <stdio.h>
-#include <string.h>
-
 
 int main() {
-    char str[101];
-    str[100] = '\0';
-    scanf("%[^\n]", str);
-    int length = strlen(str);
-    // start finding the minumum number of rotations
-    /* int sum = 0, count, start = 97, end = 122;
-    for (int k = 0; k < length; k++) {
-        count = 0;
-        if (start == 97) end = 122;
-        if (end == 122) start = 97;
-        printf("start = %d and end = %d\n", start, end);
-        for (int i = start, j = end; ; i++, j--) {
-            count++;
-            printf("i = %d, j = %d and str = %d, count = %d\n", i, j, str[k], count);
-            if (i == (int) str[k] || j == (int) str[k]) {
-                sum += count;
-                start = end = (int) str[k];
-                break;
-            }
-            if (j == 97) j = 122;
-            if (i == 122) i = 97;
-        }
-        printf("\n============\n");
-    } */
-    int start = 97;
+    char str[105];
+    scanf("%[^\n]", str); str[100] = '\0';
+    char a = 'a', max, min, v1, v2;
     int sum = 0;
-    for (int k = 0; k < length; k++) {
-        
-        
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] > a) {
+            max = str[i];  min = a;
+        } else {
+            max = a; min = str[i];
+        }
+        v1 = max - min;
+        v2 = 26 - v1;
+        if (v1 < v2) sum += v1;
+        else sum += v2;
+        a = str[i];
     }
-    
-    
     printf("%d", sum);
     return 0;
 }
-
 
 
 
