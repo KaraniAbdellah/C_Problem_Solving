6904701474d6fae8f5f1880e781cc2c7349e5179
Abdellah <AbdellahKarani>
2024-08-04 23:00:31 +0100

    stone problem

---
+++ codeforces/Colorful_Stones.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+int main() {
+    char str1[50];
+    char str2[50];
+    scanf("%s", str1); str1[49] = '\0'; getchar();
+    scanf("%s", str2); str2[49] = '\0';
+    // start getting position
+    
+    
+    
+    return 0;
+}
+
+
+
+
+
