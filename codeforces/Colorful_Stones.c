9c8ce8b9a66233656ce43a665e0775163c7d05a0
Abdellah <AbdellahKarani>
2024-08-07 00:01:23 +0100

    .

--- codeforces/Colorful_Stones.c
+++ codeforces/Colorful_Stones.c
@@ -3,14 +3,18 @@
 
 
 int main() {
-    char str1[50];
-    char str2[50];
-    scanf("%s", str1); str1[49] = '\0'; getchar();
-    scanf("%s", str2); str2[49] = '\0';
-    // start getting position
-    
-    
-    
+    char str1[51];
+    char str2[51];
+    scanf("%s", str1); str1[50] = '\0'; getchar();
+    scanf("%s", str2); str2[50] = '\0';
+    // start getting the position
+    int count = 1;
+    for (int i = 0; str2[i] != '\0'; i++) {
+        if (str1[count - 1] == str2[i]) {
+            count++;
+        }
+    }
+    printf("%d", count);
     return 0;
 }
 
