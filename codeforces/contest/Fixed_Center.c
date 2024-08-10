5c30c266e94125235eeed012d9b26c0cb14d5b2e
Abdellah <AbdellahKarani>
2024-08-10 19:19:53 +0100

    .

---
+++ codeforces/contest/Fixed_Center.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+4
+10 10 1
+0 0 3
+-5 -8 8
+4 -5 3
+
+
+int main() {
+    // start getting inputs
+    int t;
+    scanf("%d", &t);
+    int xc, yc, k;
+    int total_point = 0;
+    for (int i = 0; i < t; i++) {
+        scanf("%d %d %d", &xc, &yc, &k);
+        int sumX = 0, sumY = 0;
+        for (int j = 0; j < k; j++) {
+            for (int p = 0; p < 10000; p++) {
+                Re[k] = p - 1;
+            }
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
+
+
+
+
+
+
+
