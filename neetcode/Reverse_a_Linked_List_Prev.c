ea7f39204ffba5e33f0fcffe19bf9a3953e0f820
abdellah <abdellahkarani@gmail.com>
2024-09-16 12:46:59 +0100

    .

--- neetcode/Reverse_a_Linked_List_Prev.c
+++ neetcode/Reverse_a_Linked_List_Prev.c
@@ -6,12 +6,13 @@
 typedef struct LinkedList {
 	int data;
 	struct LinkedList *next;
+	struct LinkedList *prev;
 } LinkedList;
 
 void add_to_list(LinkedList **root, int data);
 void display_list(LinkedList *root);
-LinkedList *reverse_linked_list1(LinkedList *root);
-LinkedList *reverse_linked_list2(LinkedList *root);
+LinkedList *reverse_linked_list(LinkedList *root);
+
 
 
 
@@ -19,7 +20,7 @@ void add_to_list(LinkedList **root, int data) {
 	
 	LinkedList *new_ele = (LinkedList *) malloc(sizeof(LinkedList));
 	new_ele->data = data;
-	new_ele->next = NULL;
+	new_ele->next = new_ele->prev = NULL;
 	
 	if (*root == NULL) {
 		*root = new_ele; return;
@@ -27,6 +28,7 @@ void add_to_list(LinkedList **root, int data) {
 	
 	LinkedList *temp = *root;
 	while (temp->next != NULL) temp = temp->next;
+	new_ele->prev = temp;
 	temp->next = new_ele;
 	
 }
@@ -46,33 +48,22 @@ void display_list(LinkedList *root) {
 }
 
 
-LinkedList *reverse_linked_list1(LinkedList *root) {
+LinkedList *reverse_linked_list(LinkedList *root) {
 
 	// reverse using two pointer technique
 	if (root != NULL) {
 	
 		LinkedList *p1, *p2;
 		p1 = root; p2 = root;
-		int count = -1;
-		while (p2->next != NULL) {
-			p2 = p2->next;
-			count++;
-		} 
+		while (p2->next != NULL) p2 = p2->next; // get last ele in linked list
 		
 		int temp;
 		while (p1 < p2) {
-			
 			temp = p1->data;
 			p1->data = p2->data;
 			p2->data = temp;
-			
 			p1 = p1->next;
-			int i = 0; p2 = root;
-			while (i < count) {
-				p2 = p2->next; i++;
-			}
-			count--;
-			
+			p2 = p2->prev;
 		}
 		return root;
 		
@@ -80,29 +71,13 @@ LinkedList *reverse_linked_list1(LinkedList *root) {
 	
 }
 
-
-LinkedList *reverse_linked_list2(LinkedList *root) {
-	
-	LinkedList *p = root, *prev = NULL, *temp = root;
-	while (p != NULL) {
-		temp = p->next;
-		p->next = prev;
-		prev = p;
-		p = temp;
-	}
-	root = prev;
-	return root;
-	
-}
-
-
 int main() {
 	
 	LinkedList *root = NULL;
-    for (int i = 0; i < 9; i++) add_to_list(&root, i);
+    for (int i = 0; i < 10; i++) add_to_list(&root, i);
     
     display_list(root);
-    root = reverse_linked_list2(root);
+    root = reverse_linked_list(root);
     display_list(root);
     
     return 0;
