908059bee14a405aa4d8680d2013006f8a78cdc8
abdellah karani <abdellahkarani@gamil.com>
2024-09-24 19:33:59 +0100

    .

--- neetcode/K_Largest_Int.c
+++ neetcode/K_Largest_Int.c
@@ -2,74 +2,50 @@
 #include <stdio.h>
 
 
-
-typedef struct Tree_Right {
-	int data;
-	struct Tree_Right *right;
-	struct Tree_Right *left;
-} Tree;
-
-
-void add_int_tree(Tree **root, int data) {
+// find the Kth largest in Stream With Selection Sort Until K Times
+void find_largest1(int T[], int n, int k) {
 	
-	Tree *new_node = (Tree *) malloc(sizeof(Tree));
-	new_node->data = data;
-	new_node->right = new_node->left = NULL;
-	
-	if (*root == NULL) {
-		*root = new_node; return;
-	}
-	
-	if ((*root)->data < data) {
-		if ((*root)->right == NULL) {
-			(*root)->right = new_node; return;
-		} else {
-			free(new_node);
-			add_int_tree(&((*root)->right), data);
+	// Selection in Decroissant Order
+	int indexMin, temp;
+	for (int i = 0; i < k; i++) {
+		indexMin = i;
+		for (int j = i + 1; j < n; j++) {
+			if (T[indexMin] < T[j]) indexMin = j;
 		}
-	} else {
-		if ((*root)->left == NULL) {
-			(*root)->left = new_node; return;
-		} else {
-			free(new_node);
-			add_int_tree(&((*root)->left), data);
-		}
+		temp = T[indexMin];
+		T[indexMin] = T[i];
+		T[i] = temp;
 	}
-	
-} 
-
-
-// find the Kth largest in Stream using in_Order Traversal  
-int find_larget_in_order(Tree *root, int k, int n) {
-
-	// do this alone with in-order traversal please and understand it
-	
-	return result;	
+	printf("\nThe %d Largest Integer in Stream T is %d\n", k, T[k - 1]);
+	// Complexity is O(n * k)
 		
 }
 
 
 
+int *min_heap() {
+	
+	
+	
+}
+
+// find largest using min-heap
+void find_largest2(int T[], int n, int k) {
+	
+	
+	
+}
+
+
 int main() {
 
-	Tree *root = NULL;
 	int n = 10, k = 2;
-	int T[10] = {10, -10, 23, 23, -109, 230, 23, 32, 45}; // 45
+	int T[10] = {10, -10, 23, 23, -109, 230, 23, 32, 45};
 	
-	for (int i = 0; i < n; i++) {
-		add_int_tree(&root, T[i]);
-	}
-	
-	// printf("%d  ", root->right->data);
-	
-	printf("The %d Largest Integer in Stream T is %d\n", k, find_larget_in_order(root, k, n));
-	
-	
-	
-	
-	
+	find_largest1(T, n, k);
 	
 	return 0;
+	
 }
 
 
