0604b9b654d62394ed350b48525e5e00e12ee896
abdellah karani <abdellahkarani@gamil.com>
2024-09-25 11:31:33 +0100

    .

--- neetcode/K_Largest_Int.c
+++ neetcode/K_Largest_Int.c
@@ -22,27 +22,61 @@ void find_largest1(int T[], int n, int k) {
 }
 
 
-
-int *min_heap() {
+// T: table & k: number of eles
+int *min_heap(int *T, int k) {
 	
-	
+	int i = (k / 2) - 1;
+	void heap(int i, int *T) {
+		int min = i;
+		int g = 2 * min + 1;
+		int d = 2 * min + 2;
+		if (g < k && T[min] > T[g]) {
+			min = g;
+		}
+		if (d < k && T[min] > T[d]) {
+			min = d;
+		}
+		
+		if (min != i) {
+			int temp = T[i];
+			T[i] = T[min];
+			T[min] = temp;
+			heap(min, T);
+		}
+	}
+	for (int j = i; j >= 0; j--) {
+		heap(j, T);
+	}
+	return T;
 	
 }
 
 // find largest using min-heap
-void find_largest2(int T[], int n, int k) {
+int find_largest2(int *T, int n, int k) {
 	
+	int temp;
+	for (int i = k; i < n; i++) {
+		if (T[i] > T[0]) {
+			temp = T[0];
+			T[0] = T[i];
+			T[i] = temp;
+			min_heap(T, k);
+		}
+	}
 	
+	return T[0];
 	
 }
 
 
 int main() {
 
-	int n = 10, k = 2;
-	int T[10] = {10, -10, 23, 23, -109, 230, 23, 32, 45};
+	int n = 10, k = 1;
+	int T[10] = {1, 2, 3, 4, 4, 5, 10, 100, 19, 18}; // 1
 	
-	find_largest1(T, n, k);
+	// find_largest1(T, n, k);
+	
+	printf("The %d Largest Int In Stream T : %d\n", k, find_largest2(T, n, k));
 	
 	return 0;
 	
