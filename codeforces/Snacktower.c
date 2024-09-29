7a171c436d1de2f7da67677c00cb0e1f20fcbb46
abdellah karani <abdellahkarani@gamil.com>
2024-09-29 11:20:10 +0100

    .

--- codeforces/Snacktower.c
+++ codeforces/Snacktower.c
@@ -8,8 +8,8 @@ void sort_table(int *T, int n) {
 	int indexMin, temp;
 	for (int i = 0; i < n; i++) {
 		indexMin = i;
-		for (int j = 0; j < n; j++) {
-			if (T[indexMin] > T[j]) indexMin = j;
+		for (int j = i + 1; j < n; j++) {
+			if (T[indexMin] < T[j]) indexMin = j;
 		}
 		temp = T[indexMin];
 		T[indexMin] = T[i];
@@ -18,11 +18,21 @@ void sort_table(int *T, int n) {
 	
 }
 
+int verification(int P[], int n, int begin, int ele) {	
+	for (int i = begin; i < n; i++) {
+		if (P[i] > ele) return 1;
+	}
+	return 0;
+	
+}
+
+
 int main() {
 	
 	int n;
 	scanf("%d", &n);
 	int T[n], P[n];
+	
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &P[i]); T[i] = P[i];
 	}
@@ -30,17 +40,21 @@ int main() {
 	sort_table(T, n);
 	
 	for (int i = 0; i < n; i++) {
-		int check = 0;
-		for (int k = 0; k < n; k++) {
-			if (T[k] == P[i]) {
-				for (int j = i; j >= 0 && P[j] != -1; j--) {
-					printf("%d ", P[j]);
-					P[j] = -1;
-				}
-				printf("\n"); check = 1; break;
+		for (int j = 0; j <= i; j++) {
+			if (P[i] == T[j]) {
+				int check = verification(P, n, i + 1, P[i]);
+				if (check == 0) {
+					printf("%d ", P[i]); P[i] = -1;
+					for (int k = i - 1; k >= 0 && P[k] != -1; k--) {
+						printf("%d ", P[k]); P[k] = -1;
+					}
+					printf("\n"); break;
+				} else j = i;
+			}
+			if (j == i && P[i] != -1) {
+				printf("\n"); break;
 			}
 		}
-		if (check == 0) printf("\n");
 			
 	}
 	
