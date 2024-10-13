3f7c653217fe4cacdbf4b28c11a86bb3ce2915c6
abdellah karani <abdellahkarani@gamil.com>
2024-10-13 12:19:04 +0100

    ..

--- neetcode/merge_two_linked_list.c
+++ neetcode/merge_two_linked_list.c
@@ -5,28 +5,29 @@
 typedef struct list {
 	int data;
 	struct list *next;
-	struct list *prev;
 } list;
 
 
 void insert_at_list(list **head, int value) {
 	
 	list *new_node = (list *) malloc(sizeof(list));
-	new_node->next = new_node->prev = NULL;
+	new_node->next = NULL;
 	new_node->data = value;
-	if (head == NULL) {
+	
+	if (*head == NULL) {
 		*head = new_node; return;
 	}
 	
-	new_node->next = *head;
-	// if (*head != NULL) (*head)->prev = new_node;
-	*head = new_node;
+	list *temp = *head;
+	while (temp->next != NULL) temp = temp->next;
+	temp->next = new_node;
 	
 }
 
-void print_list(list *head) {
+void print_list(char *msg, list *head) {
 	
 	list *temp = head;
+	printf("%s", msg);
 	while (temp != NULL) {
 		printf("%d--> ", temp->data);
 		temp = temp->next;
@@ -34,13 +35,40 @@ void print_list(list *head) {
 	
 }
 
-void merge_list(list *l1, list *l2) {
+list *merge_list_copy(list *l1, list *l2) {
 	
-	if (l1 == NULL && l2 == NULL) return;
+	if (l1 == NULL && l2 == NULL) return NULL;
 	
+	list *head = NULL;
+	list *temp1 = l1, *temp2 = l2;
 	
+	while (temp1 != NULL || temp2 != NULL) {
+		
+		if (temp1 == NULL) insert_at_list(&head, temp2->data);
+		else if (temp2 == NULL) insert_at_list(&head, temp1->data);
+		else if (temp1 != NULL && temp2 != NULL && temp2->data < temp1->data) {
+			insert_at_list(&head, temp2->data);	
+			insert_at_list(&head, temp1->data);	
+		} else if (temp1 != NULL && temp2 != NULL && temp2->data > temp1->data) {
+			insert_at_list(&head, temp1->data);
+			insert_at_list(&head, temp2->data);
+		} else;
+		temp1 = temp1->next;
+		temp2 = temp2->next;
+		
+	}
+	return head;
 	
 }
+
+list *merge_list_splice(list *l1, list *l2) {
+	
+	if (l1 == NULL && l2 == NULL) return NULL;
+	
+	return NULL;
+		
+	
+} 
 
 
 int main() {
@@ -49,10 +77,12 @@ int main() {
 	list *head2 = NULL;
 	
 	insert_at_list(&head1, 1); insert_at_list(&head1, 2); insert_at_list(&head1, 3);
-	insert_at_list(&head2, 4); insert_at_list(&head2, 5); insert_at_list(&head2, 6);
+	insert_at_list(&head2, 4); insert_at_list(&head2, 5);
 	
-	print_list(head1); print_list(head2);
+	print_list("l1: ", head1); print_list("l2: ", head2);
 	
+	list *new_head = merge_list_copy(head1, head2);
+	print_list("l1 & l2 : ", new_head);
 	
 	
 	return 0;
