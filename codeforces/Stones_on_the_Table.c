116378cc12b832d28cb12c23bf7941341ae6c89a
abdellah karani <abdellahkarani@gmail.com>
2024-12-04 22:40:23 +0100

    remove comments

--- codeforces/Stones_on_the_Table.c
+++ codeforces/Stones_on_the_Table.c
@@ -8,14 +8,11 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%c", &s);
         if (i == 0) p = s;
-        // printf("We Compare %d and %d and also count = %d\n", s, p, c);
         if (s == p && i != 0) c++;
         else p = s;
     }
     printf("%d", c);
     return 0;
 }
-
-
 
 
