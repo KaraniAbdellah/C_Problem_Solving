99a44c7290ab5eeb001678cb5d0925534511c4b0
abdellah karani <abdellahkarani@gmail.com>
2024-12-07 23:03:52 +0100

    submit new solution

--- codeforces/Is_it_rated.c
+++ codeforces/Is_it_rated.c
@@ -1,1 +1,24 @@
-// get started new problem 
+#include <stdio.h>
+
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	int T[n][2];
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", T[i][0], T[i][1]);
+	}
+	
+	if (1) {
+		printf("rated");
+	} else if(1) {
+		printf("unrated");
+	} else {
+		printf("maybe");
+	}
+	
+	return 0;
+}
+
+
+
