d5942dde1bd5af6955012f03e0c3d1ad076ccf6f
abdellah karani <abdellahkarani@gmail.com>
2024-12-08 08:03:22 +0100

    submit solution for new problem

--- codeforces/Is_it_rated.c
+++ codeforces/Is_it_rated.c
@@ -6,16 +6,22 @@ int main() {
 	scanf("%d", &n);
 	int T[n][2];
 	for (int i = 0; i < n; i++) {
-		scanf("%d %d", T[i][0], T[i][1]);
+		scanf("%d %d", &T[i][0], &T[i][1]);
 	}
 	
-	if (1) {
-		printf("rated");
-	} else if(1) {
-		printf("unrated");
-	} else {
-		printf("maybe");
+	int order = 1;
+	for (int i = 0; i < n; i++) {
+		if (T[i][0] != T[i][1]) {
+			printf("rated\n"); return 0;
+		}
+		if (i < n - 1 && T[i][0] >= T[i + 1][0]) {
+			order++;
+		}
 	}
+	
+	
+	if (order == n) printf("maybe\n");
+	else printf("unrated\n");
 	
 	return 0;
 }
---
+++ codeforces/Olesya_and_Rodion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main() {
+	int n, t;
+	scanf("%d %d", &n, &t);
+	// find a number that consist n digit and devides by t
+	
+	return 0;
+}
+
+
+
