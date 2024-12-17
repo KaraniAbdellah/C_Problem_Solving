ef9c9466e5db568d05062e8ca435b6df6698d5d0
abdellah karani <abdellahkarani@gmail.com>
2024-12-17 07:56:29 +0100

    add new function for get the number of digit

--- codeforces/Olesya_and_Rodion.c
+++ codeforces/Olesya_and_Rodion.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 
 
+// find a number that consist n digit and devides by t
+
+
+int getNumberOfDigit(int n) {
+	int count = 0;
+	while(n != 0) {
+		n = n / 10;
+		count++;
+	}
+	return count; 
+}
+
+
 int main() {
-	int n, t;
+
+	int n, t, number;
 	scanf("%d %d", &n, &t);
-	// find a number that consist n digit and devides by t
+	
+	for (int i = 0; i < n; i++) {
+		if (number % 10 != 0 && getNumberOfDigit(number) == n && number % t == 0) {
+			printf("%d\n", number);
+			return;
+		}
+	}
+	printf("%d\n", -1);
 	
 	return 0;
+
 }
 
 
