1eb876b9126bc383ca62a1266d90425082c287c5
abdellah karani <abdellahkarani@gmail.com>
2024-12-20 08:01:50 +0100

    new solution

--- codeforces/Football.c
+++ codeforces/Football.c
@@ -7,13 +7,47 @@ int main() {
 	int n;
 	scanf("%d", &n);
 	
-	int team[n];
-	for (int i = 0; i < n; i++) {
-		scanf("%s", team);
+	char team[10];
+	team[9] = '\0';
+	char temp[10];
+	temp[9] = '\0';
 		
+	for (int i = 0; i < n; i++) {
+		
+		scanf("%s", team);
+		if (i != 0) {
+			int check = strcmp(team, temp);
+			if (check > 0) {
+				strcpy(temp, team);
+			} else {
+				strcpy(team, temp);
+			}
+		} else {
+			strcpy(temp, team);
+		}
 	}
 	
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
