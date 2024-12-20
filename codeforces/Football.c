46374e4bb3d2ad3617599f2a1a015c5f25e8e3d5
abdellah karani <abdellahkarani@gmail.com>
2024-12-20 08:19:16 +0100

    submit

--- codeforces/Football.c
+++ codeforces/Football.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 
 
@@ -7,25 +8,37 @@ int main() {
 	int n;
 	scanf("%d", &n);
 	
-	char team[10];
-	team[9] = '\0';
-	char temp[10];
-	temp[9] = '\0';
-		
+	char team1[10]; team1[9] = '\0';
+	char team2[10]; team2[9] = '\0';
+	
+	char teamOneFlag = '^'; 
+	char teamTwoFlag = '&';
+	char tempFlag = teamOneFlag;
+	
+	int teamOneCount = 0;
+	int teamTwoCount = 0;	
+	
 	for (int i = 0; i < n; i++) {
 		
-		scanf("%s", team);
-		if (i != 0) {
-			int check = strcmp(team, temp);
-			if (check > 0) {
-				strcpy(temp, team);
-			} else {
-				strcpy(team, temp);
-			}
+		if (i == 0) {
+			scanf("%s", team1); // QCCYXL
 		} else {
-			strcpy(temp, team);
+			scanf("%s", team2); // AXGLFQDD
+		}
+		
+		if (i != 0) {
+			if (strcmp(team1, team2) == 0 && teamOneCount >= teamTwoCount) {
+				tempFlag = teamOneFlag;
+				teamOneCount++;
+			} else {
+				tempFlag = teamTwoFlag;
+				teamTwoCount++;
+			}
 		}
 	}
+	
+	if (tempFlag == teamOneFlag) printf("%s\n", team1);
+	else printf("%s\n", team2);
 	
 	return 0;
 }
