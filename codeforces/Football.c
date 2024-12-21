898b85e0b6d6734d23ecc62c3dc3baf355a71ef7
abdellah karani <abdellahkarani@gmail.com>
2024-12-21 19:14:08 +0100

    add

--- codeforces/Football.c
+++ codeforces/Football.c
@@ -8,21 +8,26 @@ int main() {
 	int n;
 	scanf("%d", &n);
 	
-	char input[10]; input[9] = '\0';
-	char temp[10]; temp[9] = '\0';	
-	
-	
+	char *T[n];
+	char team1[n], team2[n];
 	
 	for (int i = 0; i < n; i++) {
-		
-		scanf("%s", input);
-		
-				
+		scanf("%s", T[i]);
+		if (i == 0) strcpy(team1, T[i]);
+		if (strcmp(team1, T[i]) != 0) {
+			strcpy(team2, T[i]);
+		}
 	}
 	
 	
-	if (teamOneCount >= teamTwoCount) printf("%s\n", temp);
-	else printf("%s\n", team1);
+	int c1 = 0, c2 = 0;
+	for (int i = 0; i < n; i++) {
+		if (strcmp(team1, T[i]) == 0) c1++;
+		else c2++;
+	}
+	
+	if (c1 > c2) printf("%s\n", team1);
+	else printf("%s\n", team2);
 	
 	return 0;
 }
