1c0c78cfcbe8a7cfb93036aa1e52842cfe941e59
abdellah karani <abdellahkarani@gmail.com>
2024-12-22 09:01:18 +0100

    update the solution

--- codeforces/Brain_Photos.c
+++ codeforces/Brain_Photos.c
@@ -6,25 +6,23 @@ int main() {
 	scanf("%d %d", &n, &m);
 	getchar();
 	
-	char T[n][m];
+	char c;
+	int check = 0;
 	
 	// black-and-white if it has only white, black and grey pixels in it.
 	// If there are any of cyan, magenta or yellow pixels in the photo then it is considered colored.
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			scanf("%c", &T[i][j]); getchar();
+			scanf("%c", &c); getchar();
+			if (c == 'C' || c == 'Y' || c == 'M') {
+					check = 1;
+			}
 		}
 	}	
 	
 	
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (T[i][j] == 'C' || T[i][j] == 'Y' || T[i][j] == 'M') {
-					printf("#Color"); return 0;
-			}
-		}
-	}
-	printf("#Black&White");
+	if (check) printf("#Color");
+	else printf("#Black&White");
 	
 	
 	return 0;
