a6f6472259758686fc2447c2df6816c828c5bb64
KaraniAbdellah <abdellahkarani@gmail.com>
2024-12-30 09:27:28 +0100

    add

--- codeforces/A_Valera_and_X.c
+++ codeforces/A_Valera_and_X.c
@@ -1,29 +1,20 @@
 #include <stdio.h>
 
 
-
 int check_triangle(int n, char word[n][n]) {
-
     for (int i = 0; i <= n / 2; i++) {
-        // printf("i = %d\n", i);
         for (int j = 0; j < n; j++) {
-            // printf("wil be comparse: %c %c\n", word[i][n - j - 1], word[n - i - 1][j]);
             if (j < n - i - 1) {
                 if (word[i][n - j - 1] == word[n - i - 1][j]) {
                     continue;
                 } else return 0;
-                // printf("compared: %c %c\n", word[i][n - j - 1], word[n - i - 1][j]);
             }
-
-
         }
     }
     return 1;
-
 }
 
 int check_diagonls(int n, char word[n][n]) {
-
     char temp = word[0][0];
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -34,11 +25,9 @@ int check_diagonls(int n, char word[n][n]) {
         }
     }
     return 1;
-
 }
 
 int main() {
-
     int n;
     scanf("%d", &n);
     char word[n][n];
@@ -49,20 +38,11 @@ int main() {
         }
     }
 
-
-
-
-
-
     // Check the Triangles
     int check1 = check_triangle(n, word);
 
     // Check the digonals
     int check2 = check_diagonls(n, word);
-
-
-    // printf("check1 = %d && check2 == %d\n", check1, check2);
-
 
     // print the result
     if (check1 && check2) printf("YES\n");
