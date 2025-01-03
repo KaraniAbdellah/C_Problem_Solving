cfd3bdbcc69d295b374775af8f3a2634d14d30a1
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-03 08:44:49 +0100

    ad

--- codeforces/A_Arpa_s_hard_exam_and_Mehrdad_s_naive_cheat.c
+++ codeforces/A_Arpa_s_hard_exam_and_Mehrdad_s_naive_cheat.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <math.h>
 
+
+int find_last_digit(int n) {
+    long long int re = pow(1378, n);
+    return n % 10;
+} 
 
 int main() {
-
     int n;
     scanf("%d", &n);
-    
+    printf("%d", find_last_digit(n));
+    return 0;
 }
