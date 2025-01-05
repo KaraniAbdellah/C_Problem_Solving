16428242dfcb8d00ad638a040032edb3456f7b08
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-05 22:39:47 +0100

    haapy new year

--- codeforces/A_Arpa_s_hard_exam_and_Mehrdad_s_naive_cheat.c
+++ codeforces/A_Arpa_s_hard_exam_and_Mehrdad_s_naive_cheat.c
@@ -2,10 +2,11 @@
 
 // this last
 int find_last_digit(int n) {
-   if (n % 3 == 0) return 2; 
-   else if (n % 2 == 0) return 4;
-   else if (n % 1 == 0) return 8;
-   else 0;
+    if (n == 0) return 1;
+    else if (n % 4 == 1) return 8;
+    else if (n % 4 == 2) return 4;
+    else if (n % 4 == 3) return 2;
+    else if (n % 4 == 0) return 6;
 } 
 
 int main() {
