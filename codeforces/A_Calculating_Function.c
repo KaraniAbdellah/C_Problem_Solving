9a6e26c46f0c3164be4b63f0d15322812513ee26
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-06 08:15:17 +0100

    math problem

--- codeforces/A_Calculating_Function.c
+++ codeforces/A_Calculating_Function.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 
 
-int main() {
-    int n;
-    scanf("%d", &n);
 
-    int re = 0;
-    for (int i = 1; i <= n; i++) {
-        if (n % 2 == 0) {
-            re += 1 * i;
-        } else {
-            re += -1 * i;
-        }
+int fun(int n, int *re) {
+    if (n == 0) return *re;
+    else if (n % 2 == 0) {
+        *re += n;
+        return fun(n - 1);
+    }
+    else return {
+        *re -= n;
+        return fun(n - 1);
+    }
+}
+
+
+int main() {
+    long long int n;
+    scanf("%lld", &n);
+
+    long long int re = 0;
+    for (int i = 0; i < n; i+=2) {
+        re += -1 * (i + 1);
+        if (i + 1 != n) re += 1 * (i + 2);
     }
     printf("%d", re);
     
