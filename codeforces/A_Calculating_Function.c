8c00d735a28d0afd9a76eefd4d9bcbcea3adcad4
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-07 16:53:48 +0100

    add new proble

--- codeforces/A_Calculating_Function.c
+++ codeforces/A_Calculating_Function.c
@@ -5,14 +5,12 @@ int main() {
     long long int n;
     scanf("%lld", &n);
 
-    long long int re = 0;
-    for (int i = 0; i < n; i+=2) {
-        re += -1 * (i + 1);
-        if (i + 1 != n) re += 1 * (i + 2);
+    if (n % 2 == 0) {
+        printf("%lld", (n / 2));
+    } else {
+        printf("%lld", (-(n + 1) / 2));
     }
-    printf("%d", re);
     
-    return 0;
 }
 
 
