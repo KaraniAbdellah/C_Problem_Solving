339fe2f9dbf810057b6eb0aa58a9ad41d0d07360
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-13 20:23:03 +0100

    find coprime number

--- codeforces/A_Counterexample.c
+++ codeforces/A_Counterexample.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 
-
+// CoPrime Number [if the common factor is 1]
+/*
+    find (a, b, c) where (a, b) (b, c) are coprime 
+    and (a, c) is not coprime
+        l ≤ a < b < c ≤ r
+*/
 
 int main() {
     int l, r;
     scanf("%d %d", &l, &r);
-    
+    if (r - l == 1) {
+        printf("-1\n");
+        return 0;
+    }
+
+    int a, b, c;
+    for (int i = l; i < r; i++) {
+
+    }
+
+
+
+
 
     
     return 0;
