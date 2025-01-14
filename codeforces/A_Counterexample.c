ee6a4a12be5760c58349f2e2a708a490276dc469
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-14 22:15:53 +0100

    add new solution

--- codeforces/A_Counterexample.c
+++ codeforces/A_Counterexample.c
@@ -4,7 +4,7 @@
 /*
     (a, b) and (b, c) are coprime
     (a, c) is not coprime
- */
+*/
 
 int main() {
     long long int l, r;
@@ -27,15 +27,28 @@ int main() {
     // printf("div_a = %d\n", div_a);
 
     // find c
-    for (long long int i = r; i >= a + 1; i--) {
+    for (long long int i = a + 1;; i++) {
         if (i % div_a == 0) {
             c = i; break;
         }
     }
+    // printf("c = %lld\n", c);
+
+
+    // find the division for a
+    long long int div_c;
+    k = 2;
+    while (k <= a) {
+        if (a % k == 0) {
+            div_c = k; break;
+        }
+        k++;
+    }
+
 
     // find b
     for (long long int i = a + 1; i < r && i != c; i++) {
-        if (i % div_a != 0) {
+        if (i % div_a != 0 && i % div_c != 0) {
             b = i; break;
         }
     }
