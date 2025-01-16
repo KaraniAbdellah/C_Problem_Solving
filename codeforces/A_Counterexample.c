3f8da1d7eab01fab6da240794e344dbdf67befe0
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-16 22:09:13 +0100

    add new solution

--- codeforces/A_Counterexample.c
+++ codeforces/A_Counterexample.c
@@ -9,59 +9,21 @@
 int main() {
     long long int l, r;
     scanf("%lld %lld", &l, &r);
+    
     if (r - l == 1) {
         printf("-1\n");
         return 0;
     }
 
-    // Suppose that a = l
-    long long int a = l, b, c, div_a;
-    // find the division for a
-    long long int k = 2;
-    while (k <= a) {
-        if (a % k == 0) {
-            div_a = k; break;
-        }
-        k++;
-    }
-    // printf("div_a = %d\n", div_a);
-
-    // find c
-    for (long long int i = a + 1;; i++) {
-        if (i % div_a == 0) {
-            c = i; break;
-        }
-    }
-    // printf("c = %lld\n", c);
-
-
-    // find the division for a
-    long long int div_c;
-    k = 2;
-    while (k <= a) {
-        if (a % k == 0) {
-            div_c = k; break;
-        }
-        k++;
+    if (l % 2 != 0) {
+        l++;
     }
 
-
-    // find b
-    for (long long int i = a + 1; i < r && i != c; i++) {
-        if (i % div_a != 0 && i % div_c != 0) {
-            b = i; break;
-        }
-    }
-
-    printf("%lld %lld %lld\n", a, b, c);
+    printf("%lld %lld %lld", l, l + 1, l + 2);
 
 
-
-
-
-
-    
     return 0;
+
 }
 
 
