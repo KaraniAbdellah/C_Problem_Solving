999aaf7c51fd67ba071df0dd627462e791218f67
KaraniAbdellah <abdellahkarani@gmail.com>
2025-01-19 19:45:14 +0100

    submited lattter

--- codeforces/A_Good_Number.c
+++ codeforces/A_Good_Number.c
@@ -9,39 +9,22 @@ int main() {
         printf("1");
         return 0;
     }
-    k++;
-    char Digits[k + 1];
+    // k++;
+    // char Digits[k + 1];
 
     getchar();
     for (int i = 0; i < n; i++) {
-        scanf("%[^\n]s", Digits);
-        int lenght = strlen(Digits);
-        printf("lenght = %d\n", lenght);
-        int sub_check, check;
-        sub_check = 0;
-        for (int j = 0; j < lenght; j++) {
-            check = 0;
-            printf("digit = %c\n", Digits[j]);
-            for (int p = 0; p < lenght; p++) {
-                // printf("c = %c\n", ((char) (p + 48)));
-                if ((char) (p + 48) == (int) Digits[j]) {
-                    check = 1;
-                    printf("check = %d\n", check);
-                    break;
-                }
+        scanf("%d", &x);
+        int check = 0;
+        while (x != 0) {
+            for (int j = 0; j <= k; j++) {
+                if (x % 10 == j) {
+                    check++; break;
+                } 
             }
-            if (check == 1) sub_check++;
-            else break;
+            x = x / 10;
+            if (check == 1) count++;
         }
-        printf("sub %d\n", sub_check);
-        if (sub_check == k) count++;
-        // scanf("%d", &x);
-        // int check = 0;
-        // while (x != 0) {
-        //     check++;
-        //     x = x / 10;
-        // }
-        // if (check == k + 1) count++;
     }
 
     printf("%d\n", count);
@@ -50,7 +33,27 @@ int main() {
 
 
 // Try to find a another solution that cover the part of math
-
+// scanf("%[^\n]s", Digits);
+//         int lenght = strlen(Digits);
+//         printf("lenght = %d\n", lenght);
+//         int sub_check, check;
+//         sub_check = 0;
+//         for (int j = 0; j < lenght; j++) {
+//             check = 0;
+//             printf("digit = %c\n", Digits[j]);
+//             for (int p = 0; p < lenght; p++) {
+//                 // printf("c = %c\n", ((char) (p + 48)));
+//                 if ((char) (p + 48) == (int) Digits[j]) {
+//                     check = 1;
+//                     printf("check = %d\n", check);
+//                     break;
+//                 }
+//             }
+//             if (check == 1) sub_check++;
+//             else break;
+//         }
+//         printf("sub %d\n", sub_check);
+//         if (sub_check == k) count++;
 
 
 
